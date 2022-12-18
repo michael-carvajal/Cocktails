1d8a98de41cac31bca2d8d7fc0218a3cab2037a8
michael-carvajal <michaelcarvajal.work@gmail.com>
2022-12-18 15:31:33 -0500

    add transformation

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
+const hello = 'hello cheeks'
 
 
 window.onload = () => {
-    const hello = console.log('hello cheeks');
-    if (true) {
-        hello;
-    }
 
+    const image = document.querySelector('.image');
+    const reset = document.getElementById('reset');
+    const body = document.querySelector('body');
+
+
+    reset.onclick = () => {
+        if (body.classList.contains('btn-click')) {
+            body.classList.remove('btn-click')
+        } else {
+            body.classList.add('btn-click')
+
+        }
+    }
 
 
 }
