4e9776edc365a35375ded01721402032268e9aea
michael-carvajal <michaelcarvajal.work@gmail.com>
2023-01-13 13:46:49 -0500

    added section to choose what type of liquor ingredient is wanted

--- index.js
+++ index.js
@@ -1,40 +1,48 @@
-/*const options = {
-    method: 'GET',
-    headers: {
-        'X-RapidAPI-Key': 'dc7819c2b8mshebad27cdce2062bp1f7da1jsne943fb95701a',
-        'X-RapidAPI-Host': 'the-cocktail-db.p.rapidapi.com'
-    }
-};
-
-fetch('https://the-cocktail-db.p.rapidapi.com/search.php?s=vodka', options)
-    .then(response => response.json())
-    .then(response => console.log(response.drinks[0]))
-    .catch(err => console.error(err));
-*/
+window.onload = () => {
+    const rum = document.getElementById('rum')
+    const vodka = document.getElementById('vodka');
+    const gin = document.getElementById('gin');
+    const tequila = document.getElementById('tequila');
+    const whiskey = document.getElementById('whiskey');
+    const liquors = document.getElementById('liquors');
+    const random_drink = document.getElementById('random_drink');
+    const reset = document.getElementById('reset');
+    let drink = '';
+    let multiplier;
 
 
-import React, { useState, useEffect } from 'react';
-
-function MyComponent() {
-    const [data, setData] = useState(null);
-
-    useEffect(() => {
-        async function fetchData() {
-            const response = await fetch('https://my-api.com/endpoint');
-            const data = await response.json();
-            setData(data);
+    reset.onclick = () => {
+        if (liquors.value = 'rum') {
+            multiplier = 11;
+        } else if (liquors.value = 'tequila') {
+            multiplier = 5;
+        } else if (liquors.value = 'vodka') {
+            multiplier = 9;
+        } else if (liquors.value = 'gin') {
+            multiplier = 20;
+        } else if (liquors.value = 'whiskey') {
+            multiplier = 1;
         }
-        fetchData();
-    }, []);
+        let random_number = Math.floor(Math.random() * 11)
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': 'dc7819c2b8mshebad27cdce2062bp1f7da1jsne943fb95701a',
+                'X-RapidAPI-Host': 'the-cocktail-db.p.rapidapi.com'
+            }
+        };
 
-    if (!data) {
-        return <p>Loading data...</p>;
+        fetch(`https://the-cocktail-db.p.rapidapi.com/search.php?s=${liquors.value}`, options)
+        .then(response => response.json())
+            .then(response => {
+                console.log(response.drinks[random_drink])
+            })
+        .catch(err => console.error(err));
     }
 
-    return (
-        <div>
-            <h1>{data.title}</h1>
-            <p>{data.description}</p>
-        </div>
-    );
+
+
+
+
+
 }
