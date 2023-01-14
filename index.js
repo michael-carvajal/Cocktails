e0cbccf14dde859937eb762f03d867ef9188f4a1
michael-carvajal <michaelcarvajal.work@gmail.com>
2023-01-14 00:06:37 -0500

    able to display ingredients but not able to remove the previously added ingredients

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 window.onload = () => {
     const rum = document.getElementById('rum')
     const vodka = document.getElementById('vodka');
@@ -7,11 +8,17 @@ window.onload = () => {
     const liquors = document.getElementById('liquors');
     const random_drink = document.getElementById('random_drink');
     const reset = document.getElementById('reset');
+    const ingredient_list = document.getElementById('ingredient_list');
     let drink = '';
+    let ingredients = [];
+    let image = '';
     let multiplier;
 
 
     reset.onclick = () => {
+        while (ingredient_list.firstChild) {
+            ingredient_list.removeChild(ingredient_list.firstChild);
+        }
         if (liquors.value === 'rum') {
             multiplier = 11;
         } else if (liquors.value === 'tequila') {
@@ -35,10 +42,25 @@ window.onload = () => {
         fetch(`https://the-cocktail-db.p.rapidapi.com/search.php?s=${liquors.value}`, options)
         .then(response => response.json())
             .then(data => {
+                let i = 1;
                 drink = data.drinks[random_number].strDrink;
+                while (true) {
+                    let ingredientKey = `strIngredient${i}` ;
+                    let ingredient = data.drinks[random_number][ingredientKey];
+                    if (ingredient == null) {
+                        break
+                    }
+                    ingredients.push(ingredient);
+                    i++;
+                }
             })
             .then(() => {
                 random_drink.value = drink;
+                ingredients.forEach(element => {
+                    let childDiv = document.createElement("div");
+                    childDiv.innerHTML = element;
+                    ingredient_list.appendChild(childDiv);
+                });
             })
             .catch(err => console.error(err));
 
